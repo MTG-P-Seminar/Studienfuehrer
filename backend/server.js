1d1cf5f289624d353ab89613ad8cc942aa8d8284
Fabian Leonardi <fabian@bigigloo.de>
2026-03-20 17:07:32 +0100

    Added Leni & themes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const path = require("path");
+const path = require("node:path");
 
 const app = express();
 const PORT = 3000;
 
-// 1. Statische Assets (CSS, JS, Bilder etc.)
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// 2. Dynamische Page-Routen
 app.get("/:page", (req, res) => {
   const page = req.params.page;
 
-  const filePath = path.join(
-    __dirname,
-    `../frontend/dist/pages/${page}`,
-    `index.html`
-  );
+  const filePath = path.join(__dirname, `../frontend/dist/pages/${page}`, "index.html");
 
   res.sendFile(filePath, (err) => {
     if (err) {
       res.status(404).send("Page not found");
     }
   });
+});
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, `../frontend/dist/pages/showcase`, "index.html");
+  res.sendFile(filePath);
 });
 
 app.listen(PORT, () => {
